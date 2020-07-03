95607ff2a574eccb9d9e3843a79bc72bc24264f8
Isac Sousa <isacsousadev@gmail.com>
2020-07-03 12:29:45 -0300

    Encapsulamento da classe ItemVO

--- src/br/edu/ufersa/sismart/model/VO/ItemVO.java
+++ src/br/edu/ufersa/sismart/model/VO/ItemVO.java
@@ -1,5 +1,105 @@
 package br.edu.ufersa.sismart.model.VO;
 
 public class ItemVO {
+	String marca;
+	String codigoDeBarras;
+	int quantidadeEmEstoque;
+	double preco;
+	TipoVO tipo;
+	int quantidadeCompra;
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		if (marca != null && !marca.equals("")) {
+			this.marca = marca;
+		} else {
+			System.out.println("Necessário definir um valor válido para a marca do produto.");
+		}
+	}
+	
+	public String getCodigoDeBarras() {
+			return codigoDeBarras;	
+	}
+	
+	public void setCodigoDeBarras(String codigoDeBarras) {
+		if (codigoDeBarras != null && !codigoDeBarras.equals("")) {
+			codigoDeBarras = codigoDeBarras.replaceAll("[^0-9]","");
+			if (codigoDeBarras.length() == 13) {
+				this.codigoDeBarras = codigoDeBarras;
+			} else {
+				System.out.println("Código de barras inválido.");
+			}
+		} else {
+			System.out.println("Valor inválido.");
+		}
+	}
+	
+	public int getQuantidadeEmEstoque() {
+		return quantidadeEmEstoque;
+	}
+	
+	public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+		this.quantidadeEmEstoque = quantidadeEmEstoque;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public TipoVO getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(TipoVO tipo) {
+		if (tipo != null) {
+			this.tipo = tipo;
+		} else {
+			System.out.println("Necessário definir um tipo para o produto.");
+		}		
+	}
+	
+	public int getQuantidadeCompra() {
+		return quantidadeCompra;
+	}
+	
+	public void setQuantidadeCompra(int quantidadeCompra) {
+		this.quantidadeCompra = quantidadeCompra;
+	}
+
+	@Override
+	public String toString() {
+		return "ItemVO [marca=" + marca + ", codigoDeBarras=" + codigoDeBarras + ", quantidadeEmEstoque="
+				+ quantidadeEmEstoque + ", preco=" + preco + ", tipo=" + tipo + ", quantidadeCompra=" + quantidadeCompra
+				+ "]";
+	}
+
+	public ItemVO(String marca, String codigoDeBarras, int quantidadeEmEstoque, double preco, TipoVO tipo,
+			int quantidadeCompra) {
+		setMarca(marca);
+		setCodigoDeBarras(codigoDeBarras);
+		setQuantidadeEmEstoque(quantidadeEmEstoque);
+		setPreco(preco);
+		setTipo(tipo);
+		setQuantidadeCompra(quantidadeCompra);
+	}
+	
+	
+	public ItemVO(String marca, String codigoDeBarras, TipoVO tipo) {
+		setMarca(marca);
+		setCodigoDeBarras(codigoDeBarras);
+		setTipo(tipo);
+	}
+
+	public ItemVO () {
+		
+	}
+	
 	
 }
