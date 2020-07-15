2c978e9e6d7d7c02556c1c7a5a907d8b593ba571
JacoNeto <jaconeto8@gmail.com>
2020-07-15 18:26:43 -0300

    Implementação das classes FuncionarioDAO e GerenteDAO

---
+++ src/br/edu/ufersa/sismart/model/DAO/FuncionarioDAO.java
@@ -0,0 +1,37 @@
+package br.edu.ufersa.sismart.model.DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import br.edu.ufersa.sismart.model.VO.FuncionarioVO;
+
+public class FuncionarioDAO extends UsuarioDAO<FuncionarioVO>{
+	
+	@Override
+	public void inserir(FuncionarioVO value) {
+		try {
+			super.inserir(value);
+			String sql = "insert into gerente (id_pessoa, id_usuario) values (?,?)";
+			PreparedStatement ptst;
+			ptst = getConnection().prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+			ptst.setLong(1, value.getIdPessoa());
+			ptst.setLong(2, value.getIdUsu());
+			int affectedRows = ptst.executeUpdate();
+			if (affectedRows == 0) {
+	        	throw new SQLException("A inserção falhou. Nenhuma linha foi alterada.");
+		    }
+		    ResultSet generatedKeys = ptst.getGeneratedKeys();
+		    if (generatedKeys.next()) {
+		         value.setId(generatedKeys.getLong(1));
+		    }
+		    else {
+		       throw new SQLException("A inserção falhou. Nenhum id foi retornado.");
+		    }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- src/br/edu/ufersa/sismart/model/VO/GerenteVO.java
+++ src/br/edu/ufersa/sismart/model/VO/GerenteVO.java
@@ -1,5 +1,13 @@
 package br.edu.ufersa.sismart.model.VO;
 
 public class GerenteVO extends UsuarioVO{
-	
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 }
