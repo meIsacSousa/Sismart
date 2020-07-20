94ba3249946331dafcfc33b73e2f0c4b0eb9108e
JacoNeto <jaconeto8@gmail.com>
2020-07-20 08:32:35 -0300

    Adição das telas Cadastrar e Editar Item
    Também foram adicionadas as imagens necessárias para a view do Sistema.

---
+++ src/br/edu/ufersa/sismart/view/TelaCadastroItens.java
@@ -0,0 +1,22 @@
+package br.edu.ufersa.sismart.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TelaCadastroItens extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("VE/telaCadastroDeItens.fxml"));
+		
+		Scene cena = new Scene(root);
+		
+		primaryStage.setTitle("Sismart");
+		primaryStage.setScene(cena);
+		primaryStage.show();
+		
+		
+	}
+}
